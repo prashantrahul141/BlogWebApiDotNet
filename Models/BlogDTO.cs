116b56cf413821cd008d0248eacb89c29fe29789
Prashant <prashantrahul141@protonmail.com>
2024-04-30 03:24:56 +0530

    add BlogDTOReturn

--- Models/BlogDTO.cs
+++ Models/BlogDTO.cs
@@ -4,4 +4,18 @@
         public required string Body { get; set; }
 
     }
+
+    public class BlogDTOReturn {
+        public required string UserId { get; set; }
+        public required string CreatedAt { get; set; }
+
+        public required string Title { get; set; }
+        public required string Body { get; set; }
+
+        BlogDTOReturn() { }
+
+        public static BlogDTOReturn FromBlog(Blog b) {
+            return new BlogDTOReturn { Body = b.Body, Title = b.Title, UserId = b.Userid ?? "", CreatedAt = b.Createdat.ToString() };
+        }
+    }
 };
