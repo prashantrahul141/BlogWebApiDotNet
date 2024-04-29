6c8dd6789b8935f6ebc9d18477c25c65699ddd7a
Prashant <prashantrahul141@protonmail.com>
2024-04-30 03:24:04 +0530

    add UserDTOReturn

--- Models/UserDTO.cs
+++ Models/UserDTO.cs
@@ -5,5 +5,17 @@
         public required string Email { get; set; }
 
         public required string Password { get; set; }
+
+        public string Image { get; set; } = string.Empty;
+    }
+
+    public class UserPublicDTO {
+        public required string userId { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public string Image { get; set; } = string.Empty;
     }
 };
