798ca165cc5b38128948f0a39f893110f8744e3c
Prashant <prashantrahul141@protonmail.com>
2024-04-30 20:43:55 +0530

    add Image to UserModel

--- src/User.cs
+++ src/User.cs
@@ -3,11 +3,10 @@
 namespace BlogWebApiDotNet {
     public partial class User : IdentityUser {
 
+        public string Image { get; set; } = string.Empty;
         public virtual ICollection<Blog> Blogs { get; set; } = [];
 
         public User() { }
-
-
 
     }
 }
