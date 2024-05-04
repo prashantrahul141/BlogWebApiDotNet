83cc950b285055c0ec40516a84d66106cfb77efc
Prashant <prashantrahul141@protonmail.com>
2024-05-04 14:25:03 +0530

    update DataContext AND BlogDTO to fit new registation

--- Models/BlogDTO.cs
+++ Models/BlogDTO.cs
@@ -6,16 +6,27 @@
     }
 
     public class BlogDTOReturn {
+        public required long Id { get; set; }
         public required string UserId { get; set; }
+
+        public required string UserName { get; set; }
         public required string CreatedAt { get; set; }
 
         public required string Title { get; set; }
         public required string Body { get; set; }
 
+
         BlogDTOReturn() { }
 
         public static BlogDTOReturn FromBlog(Blog b) {
-            return new BlogDTOReturn { Body = b.Body, Title = b.Title, UserId = b.Userid ?? "", CreatedAt = b.Createdat.ToString() };
+            var Username = "unknown";
+            if (b.User != null) {
+                Username = b.User.UserName;
+            }
+
+#pragma warning disable CS8601 //  coulnt be null because we just checked the existence of user
+            return new BlogDTOReturn { Id = b.Id, Body = b.Body, Title = b.Title, UserId = b.Userid ?? "", UserName = Username, CreatedAt = b.Createdat.ToString() };
+#pragma warning restore CS8601 // Possible null reference assignment.
         }
     }
 };
--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogWebApiDotNet.Models {
-    public class DataContext(DbContextOptions<DataContext> options) : IdentityDbContext<User>(options) {
+    public class DataContext(DbContextOptions<DataContext> options) : IdentityDbContext<AppUser>(options) {
         public virtual DbSet<Blog> Blogs { get; set; }
     }
 
