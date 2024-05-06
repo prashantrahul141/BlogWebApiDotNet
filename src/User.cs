42de7f7aae7304f4e732bc5d4391593b2edfd93c
Prashant <prashantrahul141@protonmail.com>
2024-05-06 13:24:37 +0530

    default userImage

--- src/User.cs
+++ src/User.cs
@@ -2,7 +2,7 @@
 
 namespace BlogWebApiDotNet {
     public partial class AppUser : IdentityUser {
-        public string Image { get; set; } = string.Empty;
+        public string Image { get; set; } = "https://avatars.githubusercontent.com/u/59825803?v=4";
         public virtual ICollection<Blog> Blogs { get; set; } = [];
 
         public AppUser() {
